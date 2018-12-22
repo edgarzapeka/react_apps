617ecc9b148deb62d263b9c0a8093b9cfaf5819b
edgarzapeka <edgar.zapeka@gmail.com>
2018-12-22 01:15:11 -0800

    updated Book component

--- thebookstore/src/components/Book.js
+++ thebookstore/src/components/Book.js
@@ -1,20 +1,29 @@
 import React from 'react';
 import { Card, CardImg, CardText, CardBody, 
-    CardTitle, CardSubtitle, Button, Col } from 'reactstrap';
+    CardTitle, CardHeader, CardSubtitle, CardLink, Col } from 'reactstrap';
 
 import { bookItem } from './../styles/componenets/book_list.scss';
 
 const Book = props => {
-    const { title, description } = props.book;
+    const { title, description, author, price, createdAt, updatedAt } = props.book;
+    console.log(props.book)
 
     return (
         <Col md="3" className={bookItem}>
             <Card>
-                <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="book image"/>
+                <CardHeader>{title}</CardHeader>
+                {//<CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="book image"/>
+                }
                 <CardBody>
-                    <CardTitle>{title}</CardTitle>
+                    <CardTitle>Author: {author}</CardTitle>
+                    <CardSubtitle>Price: {price}</CardSubtitle>
                     <CardText>{description}</CardText>
-                    <Button>See more</Button>
+                    <CardLink href="#">See more</CardLink>
+                    {updatedAt && (
+                        <CardText>
+                            <small className="text-muted">Last updated 3 mins ago</small>
+                        </CardText>
+                    )}
                 </CardBody>
             </Card>
         </Col>
